5d2e93efc8048c8ecff3e7b392af4397e815fc0c
BorisP1996 <prposboris13@hotmail.com>
2020-06-25 10:03:27 +0200

    add song and read

--- Dan_XXXIX_Boris_Prpos/Zadatak_1/Program.cs
+++ Dan_XXXIX_Boris_Prpos/Zadatak_1/Program.cs
@@ -10,6 +10,37 @@ namespace Zadatak_1
     {
         static void Main(string[] args)
         {
+            Song song = new Song();
+            bool spin = true;
+            while (spin)
+            {
+                Console.WriteLine("---Welcome to audio player---\n");
+                Console.WriteLine("1: Add song");
+                Console.WriteLine("2: View songs");
+                Console.WriteLine("3: Start audio player");
+                Console.WriteLine("4: Exit");
+
+                string pick = Console.ReadLine();
+                switch (pick)
+                {
+                    case "1":
+                        song.AddSong();
+                        break;
+                    case "2":
+                        song.ReadFromFile();
+                        break;
+                    case "3":
+                        Console.WriteLine("player started");
+                        break;
+                    case "4":
+                        spin = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input. Please select one option from the list.");
+                        break;
+                }
+            }
         }
     }
 }
---
+++ Dan_XXXIX_Boris_Prpos/Zadatak_1/Song.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    class Song
+    {
+        string Author { get; set; }
+        string Name { get; set; }
+        string Duration { get; set; }
+        string path = @"../../Music.txt";
+
+        public Song()
+        {
+
+        }
+        public Song(string a, string n, string d)
+        {
+            Author = a;
+            Name = n;
+            Duration = d;
+        }
+        public void AddSong()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter song author or press ~ to exit:");
+                string inputAuthor = Console.ReadLine();
+                if (inputAuthor=="~")
+                {
+                    break;
+                }
+                while (String.IsNullOrEmpty(inputAuthor))
+                {
+                    Console.WriteLine("Invalid input. Please try again:");
+                    inputAuthor = Console.ReadLine();
+                }
+                Console.WriteLine("Enter song name or press ~ to exit:");
+                string inputName = Console.ReadLine();
+                if (inputName=="~")
+                {
+                    break;
+                }
+                while (String.IsNullOrEmpty(inputName))
+                {
+                    Console.WriteLine("Invalid input. Please try again:");
+                    inputName = Console.ReadLine();
+                }
+                Console.WriteLine("Enter song duration in format hh:mm:ss or press ~ to exit:");
+                string inputDuration = Console.ReadLine();
+                if (inputDuration=="~")
+                {
+                    break;
+                }
+                while (DurationValidation(inputDuration)==false)
+                {
+                    Console.WriteLine("Please enter song duration in correct format. Try again:");
+                    inputDuration = Console.ReadLine();
+                }
+                Song s = new Song(inputAuthor, inputName, inputDuration);
+                Console.WriteLine("Song is created!");
+                Console.WriteLine(s.SongDisplay(s));
+
+                s.WriteToFile(s);
+                Console.WriteLine("Song is added to the music.txt file.");
+            }
+        }
+        public void ReadFromFile()
+        {
+            StreamReader sr = new StreamReader(path);
+            List<string> songList = new List<string>();
+            string line = "";
+            while ((line=sr.ReadLine())!=null)
+            {
+                songList.Add(line);
+            }
+            sr.Close();
+
+            if (songList.Count==0)
+            {
+                Console.WriteLine("There are not any songs in file.");
+            }
+            else
+            {
+                foreach (string item in songList)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+        public void WriteToFile(Song s)
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+
+            sw.WriteLine(SongDisplay(s));
+
+            sw.Close();
+        }
+        public string SongDisplay(Song s)
+        {
+             string write = "["+s.Author+"]:["+s.Name+"] ["+s.Duration+"]" ;
+            return write;
+        }
+        public bool DurationValidation(string duration)
+        {
+            char[] array = duration.ToCharArray();
+
+            if (array.Count() ==8 && Char.IsDigit(array[0]) && Char.IsDigit(array[1]) && array[2]==':' && Char.IsDigit(array[3]) && Char.IsDigit(array[4]) && array[5]==':' && Char.IsDigit(array[6]) && Char.IsDigit(array[7]) )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+
+    }
+
+}
